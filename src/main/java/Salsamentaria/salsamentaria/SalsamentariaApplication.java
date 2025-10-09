5271055b18bc2abfc1dc6ef85b59de9e9ea2bd87
Juan <juanbarrios072@gmail.com>
2025-10-09 15:19:35 -0500

    Creación del proyecto con dependencias

---
+++ src/main/java/Salsamentaria/salsamentaria/SalsamentariaApplication.java
@@ -0,0 +1,13 @@
+package Salsamentaria.salsamentaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalsamentariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalsamentariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Salsamentaria/salsamentaria/SalsamentariaApplicationTests.java
@@ -0,0 +1,13 @@
+package Salsamentaria.salsamentaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalsamentariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
